386e5bedfda4cd0e2982bc573d501b16f2aae804
megwillis <meg3willis@gmail.com>
2023-12-13 08:00:46 +1000

    2-3 days of watching the youtube video

---
+++ src/_data/news.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=au&apiKey=${process.env.API_KEY}&pagesize=5`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
