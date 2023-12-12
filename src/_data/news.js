4b95a05d2cb28906adad622a8bffce68f25a48a0
megwillis <meg3willis@gmail.com>
2023-12-13 08:05:01 +1000

    Update news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 
+require('dotenv').config();
+
 module.exports = async function() {
     try {
       const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=au&apiKey=${process.env.API_KEY}&pagesize=5`);
@@ -7,4 +9,4 @@ module.exports = async function() {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
